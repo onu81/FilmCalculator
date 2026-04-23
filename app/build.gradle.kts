73151d1907451532bb9dcf28cfa49c452144c48b
onu81 <wjdrnsaos@gmail.com>
2026-04-23 22:18:37 +0900

    Web: 루트 폴더에 정문(index.html) 설치 완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.onu81.interiorfilmcalc"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.onu81.interiorfilmcalc"
